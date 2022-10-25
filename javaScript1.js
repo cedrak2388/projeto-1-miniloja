11683c82ef75fd66b98409d810e9e858de811d13
cedrak2388 <77256298+cedrak2388@users.noreply.github.com>
2022-10-24 22:35:46 -0300

    Add files via upload

---
+++ javaScript1.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado(a) ' + nome.value + ' cadastro efetuado com sucesso!');
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,1 @@
+primeira entrega referente a disciplina de programação para web
